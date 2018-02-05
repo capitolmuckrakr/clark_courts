3cc4c776c71d4f7ece48698aa114bc1e38fb8f4c
capitolmuckrakr <alex@capitolmuckraker.com>
2018-02-05 10:21:26 -0800

    Revert "fix missing element divDocumentsInformation_body"
    This reverts commit 0668ec4997335be78a796b7273ddd0b17d6034fb.

--- clark_district_court_firefox.py
+++ clark_district_court_firefox.py
@@ -63,9 +63,7 @@ class Browser:
         self.search(search_string)
         sleep(5)
         section = lambda: self.driver.find_element_by_id("divDocumentsInformation_body")
-        section_fnd = section()
-        sleep(5)
-        if section_fnd:
+        if section():
             docs = lambda: section().find_elements_by_tag_name('p')
             link = ''
             downloaded = False
