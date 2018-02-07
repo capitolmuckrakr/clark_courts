640848dd4364a52d3f15390594fc3b90d0349f76
capitolmuckrakr <alex@capitolmuckraker.com>
2018-02-07 15:58:53 -0800

    download files for ~1 hour, close and sleep for 10 mins, repeat

--- parse_cases.py
+++ parse_cases.py
@@ -3,6 +3,8 @@ from clark_district_court_firefox import Browser
 
 from time import sleep
 
+from datetime import datetime,timedelta
+
 import random, os, csv
 
 def sorted_dir(folder):
@@ -24,38 +26,52 @@ most_recent_tiff=lambda x: sorted_dir(x)[0].split('.')[0]
 
 case_nums=[x.strip() for x in open(cases).readlines()[1:]]
 
-limit=0 # don't download previously saved cases
-
-browser = Browser()
-
-most_recent_index=case_nums.index(most_recent_tiff(download_dir))
-
-with open(cases) as f:
+while True:
     
-    reader = csv.reader(f)
+    start=datetime.now()
+    
+    end=start+timedelta(hours=1)
 
-    for row in reader:
+    limit=0 # don't download previously saved cases
+    
+    browser = Browser()
+    
+    most_recent_index=case_nums.index(most_recent_tiff(download_dir))
+    
+    with open(cases) as f:
         
-        if limit<=most_recent_index:
-
-            limit+=1
-
-            continue
-
-        else:
-
-            if len(row)>0:
+        reader = csv.reader(f)
+    
+        for row in reader:
             
-                x = random.randint(3,53)
-            
-                y = random.randint(1,10)
-            
-                z = x+y
-            
-                case_number = row[0]
-            
-                browser.download(case_number)
-
-                sleep(z)
-            
-browser.close()
+            if limit<=most_recent_index:
+    
+                limit+=1
+    
+                continue
+    
+            else:
+    
+                if len(row)>0:
+                
+                    x = random.randint(3,53)
+                
+                    y = random.randint(1,10)
+                
+                    z = x+y
+                
+                    case_number = row[0]
+                
+                    browser.download(case_number)
+                    
+                    if end<=datetime.now():
+                        
+                        break
+                    
+                    else:
+                        
+                         sleep(z)
+                
+    browser.close()
+    
+    sleep(600)
